e3255cf59e018d4ca581a5541b13a9acd9465c12
Sindhura Gollamudi <gollamudisindhura1@gmail.com>
2026-01-14 16:33:09 -0500

    Completed Lab. connected to the database

--- server.js
+++ server.js
@@ -3,5 +3,19 @@ const express = require("express");
 const { MongoClient } = require('mongodb');
 const app = express();
 const uri = process.env.MONGO_URI;
+
+const PORT = process.env.PORT || 3001;
 const client = new MongoClient(uri);
 
+app.get('/', async (req, res) => {
+    try {
+        await client.connect();
+        res.json({ message: "Successfully connected to the database!" });
+    } catch(err) {
+        res.status(500).json({ message: "Failed to connect to the database." });
+    }
+});
+
+app.listen(PORT, ()=> {
+    console.log(`Server is running at http://localhost:${PORT}`)
+});
